ae42adf1c8ecf2ad04d0c60a7ae6940efbd69a61
paulo <paulosheliga@gmail.com>
2026-02-07 19:43:34 -0300

    feat: implementação do metodo de reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,17 +15,23 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite == null)
+            {
+                throw new InvalidOperationException("A suíte deve ser cadastrada antes de cadastrar hóspedes.");
+            }
+
+            if (hospedes == null)
+            {
+                throw new ArgumentNullException(nameof(hospedes));
+            }
+
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
+                return;
             }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
+
+            throw new ArgumentException("A capacidade da suíte é menor que a quantidade de hóspedes.");
         }
 
         public void CadastrarSuite(Suite suite)
@@ -35,23 +41,21 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes?.Count ?? 0;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite == null)
             {
-                valor = 0;
+                throw new InvalidOperationException("A suíte deve ser cadastrada antes de calcular o valor da diária.");
+            }
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9m;
             }
 
             return valor;
